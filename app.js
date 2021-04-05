b9043494f6844f4430214ed5e5980601d432cb7e
Execlis <anreaf13@gmail.com>
2021-04-05 19:03:03 +0300

    creation of snake

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ let circle = function (x, y, radius, fillCircle) {
   }
 };
 
-// creation of objects 
+// creation of objects
 
 let Block = function (col, row) {
   this.col = col;
@@ -78,9 +78,6 @@ Block.prototype.drawSquare = function (color) {
   context.fillRect(x, y, blockSize, blockSize);
 };
 
-let sampleBlock = new Block(6, 4);
-sampleBlock.drawSquare("Blue");
-
 Block.prototype.drawCircle = function (color) {
   let centerX = this.col * blockSize + blockSize / 2;
   let centerY = this.row * blockSize + blockSize / 2;
@@ -88,12 +85,71 @@ Block.prototype.drawCircle = function (color) {
   circle(centerX, centerY, blockSize / 2, true);
 };
 
-let sampleCircle = new Block(0, 0);
-sampleCircle.drawCircle("Green");
-
-Block.prototype.equal = function (otherBlock ) {
+Block.prototype.equal = function (otherBlock) {
   return this.col === otherBlock.col && this.row === otherBlock.Block.row;
 };
 
 // creation of snake
 
+let Snake = function () {
+  this.segments = [new Block(7, 5), new Block(6, 5), new Block(5, 5)];
+  this.direction = "right";
+  this.nextDirection = "right";
+};
+
+Snake.prototype.draw = function () {
+  for (let i = 0; i < this.segments.length; i++) {
+    this.segments[i].drawSquare("Blue");
+  }
+};
+
+Snake.prototype.move = function () {
+  let head = this.segments[0];
+  let newHead;
+
+  this.direction = this.nextDirection;
+
+  if (this.direction === "right") {
+    newHead = new Block(head.col + 1, head.row);
+  } else if (this.direction === "down") {
+    newHead = new Block(head.col, head.row + 1);
+  } else if (this.direction === "left") {
+    newHead = new Block(head.col - 1, head.row);
+  } else if (this.direction === "up") {
+    newHead = new Block(head.col, head.row - 1);
+  }
+
+  if (this.checkCollision(newHead)) {
+    gameOver();
+    return;
+  }
+
+  this.segments.unshift(newHead);
+
+  if (newHead.equal(apple.position)) {
+    score++;
+    apple.move();
+  } else {
+    this.segments.pop();
+  }
+};
+
+Snake.prototype.checkCollision = function (head) {
+  let leftCollision = (head.col === 0);
+  let topCollision = (head.row === 0);
+  let rightCollision = (head.col === widthBlock - 1);
+  let bottomCollision = (head.col === heightBlock - 1);
+
+  let wallCollision = leftCollision || topCollision ||
+    rightCollision || bottomCollision;
+
+  let selfCollision = false;
+
+  for (let i = 0; i < this.segments.length; i++) {
+    if (head.equal(this.segments[i])) {
+      selfCollision = true;
+    }
+  }
+
+  return wallCollision || selfCollision;
+}
